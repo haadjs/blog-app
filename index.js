26398912448920e4bfd6a0952cb30eafc08e2854
Haad <haaddev@gmail.com>
2025-02-06 20:11:05 +0500

    done

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-import {db} from './Main/config.js'
-import { getDocs ,doc,collection } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
-
-let userName = document.querySelector('#Username')
-
-let getuserName = async ( ) =>{
-    const querySnapshot = await getDocs(collection(db, "userData"));
-querySnapshot.forEach((doc) => {
-  console.log(doc.data());
-  userName.innerHTML = doc.data().username;
-});
+import { db } from "./Main/config.js";
+import {
+  getDocs,
+  doc,
+  collection,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
 
 
-}
-getuserName()
+// itnitialize all Variable
+let Data = [];
+let userName = document.querySelector("#Username");
+let imgSrc = document.querySelector("#profileImg");
+let logBtn = document.querySelector('#logBtn')
 
+// Logout Function
+let login = true;
 
-
-
-
-
-
-
-
+logBtn.addEventListener('click', () => {
+    if (login === true){
+    
+    }
+})
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+// Get the image and the name of the user
+let getuserName = async () => {
+  const querySnapshot = await getDocs(collection(db, "userData"));
+  querySnapshot.forEach((doc) => {
+    Data.push(doc.data());
+    userName.innerHTML = doc.data().username;
+    imgSrc.src = doc.data().userProfile;
+  });
+};
+getuserName();
