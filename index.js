ef525c1cf415d3cfeb707b69b28eeac86e073f9a
Haad <haaddev@gmail.com>
2025-02-08 19:19:30 +0500

    done

--- index.js
+++ index.js
@@ -1,65 +1,62 @@
-import { db,auth } from "./Main/config.js";
+import { db, auth } from "./Main/config.js";
 import {
   getDocs,
   doc,
   collection,
-  where ,
-  query
+  where,
+  query,
 } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
-import {signOut , onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js'
+import {
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
     const uid = user.uid;
-    console.log(uid); 
-    
+    console.log(uid);
+  
   } else {
     // No user is signed in.
     window.location.href = "/Auth/login.html";
   }
 });
 
-
 // itnitialize all Variable
 let Data = [];
 let userName = document.querySelector("#Username");
 let imgSrc = document.querySelector("#profileImg");
-let logoutBtn = document.querySelector('#logBtn')
-let logInBtn = document.querySelector('#logInBtn')
-
+let logoutBtn = document.querySelector("#logBtn");
+let logInBtn = document.querySelector("#logInBtn");
 
 // Logout Function
 let login = true;
 
 if (!login) {
-    logInBtn.style.display = 'none';
-    logoutBtn.style.display = 'block';
-}else if (login){
-    logInBtn.style.display = 'none';
-    logoutBtn.style.display = 'block';
+  logInBtn.style.display = "block";
+  logoutBtn.style.display = "none";
+} else{
+  logInBtn.style.display = "none";
+  logoutBtn.style.display = "block";
 }
 
-logoutBtn.addEventListener('click', () => {
-    signOut(auth).then(() => {
-        window.location.href = "/Auth/login.html";
-      }).catch((error) => {
-        // An error happened.
-      });
-})
-
-
-
+logoutBtn.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      window.location.href = "/Auth/login.html";
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+});
 
 // Get the image and the name of the user
-let getuserName = async () => {
-  
-  const q = query(collection(db, "userData"), where("userUid", "==", auth.currentUser.uid));
-
-  const querySnapshot = await getDocs(q);
+let getuserName = async ( ) => {
+  const querySnapshot = await getDocs(collection(db, "userData"));
   querySnapshot.forEach((doc) => {
-    Data.push(doc.data());
-    userName.innerHTML = ` Welcome,${doc.data().username}`;
+    console.log(doc.data());
+    userName.innerHTML = doc.data().username;
     imgSrc.src = doc.data().userProfile;
   });
-};
-getuserName();
+}
+getuserName()
