2ccab7543ad58fefe35dc0471f8d3a05aea5c620
Haad <haaddev@gmail.com>
2025-02-13 01:30:53 +0500

    done

--- Auth/login.js
+++ Auth/login.js
@@ -1,4 +1,4 @@
-import { db, auth } from "./Main/config.js";
+import { db, auth } from "/Main/config.js";
 import {
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
--- Auth/register.js
+++ Auth/register.js
@@ -1,4 +1,4 @@
-import { auth, db } from "/Main/config.js";
+import { auth, db } from "./Main/config.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
